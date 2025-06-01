269bacb55c61746bba7fb738bc5a7c6427a2f97a
Jan <janp1krc@gmail.com>
2025-06-01 23:47:11 +0200

    First works on app look

---
+++ src/components/CreateTreeButton.tsx
@@ -0,0 +1,44 @@
+import "./CreateTreeButton.css"
+import logo from '../assets/tree.png'
+
+export default function CreateTreeButton() {
+  return (
+    <div className="main-menu-container">
+      <img src={logo} alt="Logo" className="tree-logo"/>
+      
+      <div className="menu-button-container">
+        <button className="animated-button aqua-button">
+          <svg viewBox="0 0 24 24" className="arr-2" >
+            <path
+              d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+            ></path>
+          </svg>
+          <span className="text">Manage Trees</span>
+          <span className="circle"></span>
+          <svg viewBox="0 0 24 24" className="arr-1" >
+            <path
+              d="M16.1716 10.9999L10.8076 5.63589L12.2218 4.22168L20 11.9999L12.2218 19.778L10.8076 18.3638L16.1716 12.9999H4V10.9999H16.1716Z"
+            ></path>
+          </svg>
+        </button>
+        <button className="animated-button green-button">
+          <svg viewBox="0 0 24 24" className="arr-2" >
+            <path d="M12 5v14M5 12h14" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
+          </svg>
+          <span className="text">Create Tree</span>
+          <span className="circle"></span>
+          <svg viewBox="0 0 24 24" className="arr-1" >
+            <path d="M12 5v14M5 12h14" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
+          </svg>
+        </button>
+      </div>
+    </div>
+  )
+}
+
+
+
+<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M12 5v14M5 12h14" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
+</svg>
+
---
+++ src/components/FamilyTreeCanvas.tsx
@@ -0,0 +1,11 @@
+import './FamilyTreeCanvas.css'
+
+export default function FamilyTreeCanvas() {
+  return (
+    <div className="canvas-page-container">
+
+
+    </div>
+  )
+}
+
